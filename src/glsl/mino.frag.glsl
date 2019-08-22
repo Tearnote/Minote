400a8742ed710c6059b0af1c1e3ed95fab923cdf
Hubert Maraszek <marach5@gmail.com>
2019-08-22 14:54:09 +0100

    Add specular shading

--- src/glsl/mino.frag.glsl
+++ src/glsl/mino.frag.glsl
@@ -11,12 +11,19 @@ void main()
 	vec3 normal = normalize(fNormal);
 	vec3 lightPosition = vec3(2.0, 5.0, 0.0);
 	vec3 lightColor = vec3(1.0, 1.0, 1.0);
-	vec3 lightDirection = normalize(lightPosition - fPosition);
+	vec3 lightDirection = normalize(fPosition - lightPosition);
 
 	float ambientStrength = pow(0.1, 2.2);
 	vec3 ambient = ambientStrength * lightColor;
-	float diffuseStrength = max(dot(normal, lightDirection), 0.0);
+
+	float diffuseStrength = max(dot(normal, -lightDirection), 0.0);
 	vec3 diffuse = diffuseStrength * lightColor;
 
-	outColor = vec4(ambient + diffuse, 1.0) * fColor;
+	float specularStrength = pow(0.5, 2.2);
+	vec3 viewDirection = normalize(-fPosition);
+	vec3 reflectDirection = reflect(lightDirection, normal);
+	float spec = pow(max(dot(viewDirection, reflectDirection), 0.0), 32);
+	vec3 specular = specularStrength * spec * lightColor;
+
+	outColor = vec4(ambient + diffuse + specular, 1.0) * fColor;
 }
