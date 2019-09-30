0eca9a0e566f705c450dd2bcf068884d2b830ba1
Hubert Maraszek <marach5@gmail.com>
2019-09-30 23:04:59 +0100

    Replace bloom with a more efficient algorithm

--- src/glsl/blur.frag.glsl
+++ src/glsl/blur.frag.glsl
@@ -5,11 +5,11 @@ in vec2 fTexCoords;
 out vec4 outColor;
 
 uniform sampler2D screen;
-uniform int step;
+uniform float step;
 
 void main()
 {
-	vec2 step1 = (vec2(step) + 0.5) / textureSize(screen, 0);
+	vec2 step1 = vec2(step) / textureSize(screen, 0);
 	vec4 color = vec4(0.0, 0.0, 0.0, 0.0);
 	color += texture(screen, fTexCoords + step1) / 4.0;
 	color += texture(screen,  fTexCoords - step1) / 4.0;
--- src/postrender.c
+++ src/postrender.c
@@ -14,6 +14,9 @@
 #include "window.h"
 #include "timer.h"
 
+#define BLOOM_PASSES 4
+#define BLOOM_SIZE 720
+
 #define VIGNETTE_BASE 0.4f
 #define VIGNETTE_MAX 0.46f
 #define VIGNETTE_PULSE (0.9 * SEC)
@@ -26,8 +29,7 @@ static GLuint resolveFbo = 0;
 static GLuint resolveFboColor = 0;
 
 static GLuint bloomFbo = 0;
-static GLuint bloomFboColor1 = 0;
-static GLuint bloomFboColor2 = 0;
+static GLuint bloomFboColor[BLOOM_PASSES] = {};
 
 static GLuint thresholdProgram = 0;
 static GLint thresholdAttr = -1;
@@ -59,10 +61,7 @@ static int fboHeight = DEFAULT_HEIGHT;
 static int bloomWidth = DEFAULT_WIDTH;
 static int bloomHeight = DEFAULT_HEIGHT;
 
-static int blurKernel[] = { 0, 1, 2, 3, 4, 5, 7, 8, 9, 10 };
-
 static float falloff = VIGNETTE_BASE;
-
 static nsec vignettePulseStart = -1;
 static float vignettePulseSpeed = 1.0f;
 
@@ -155,21 +154,22 @@ void initPostRenderer(void)
 	glBindTexture(GL_TEXTURE_2D, resolveFboColor);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glBindTexture(GL_TEXTURE_2D, 0);
 
 	// Create the bloom framebuffers
 	glGenFramebuffers(1, &bloomFbo);
 
-	glGenTextures(1, &bloomFboColor1);
-	glBindTexture(GL_TEXTURE_2D, bloomFboColor1);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glBindTexture(GL_TEXTURE_2D, 0);
-
-	glGenTextures(1, &bloomFboColor2);
-	glBindTexture(GL_TEXTURE_2D, bloomFboColor2);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glGenTextures(BLOOM_PASSES, bloomFboColor);
+	for (int i = 0; i < BLOOM_PASSES; i++) {
+		glBindTexture(GL_TEXTURE_2D, bloomFboColor[i]);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+		                GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,
+		                GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,
+		                GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T,
+		                GL_CLAMP_TO_EDGE);
+	}
 	glBindTexture(GL_TEXTURE_2D, 0);
 
 	// Initialize the framebuffer attachments
@@ -197,10 +197,9 @@ void initPostRenderer(void)
 	}
 
 	glBindFramebuffer(GL_FRAMEBUFFER, bloomFbo);
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
-	                       GL_TEXTURE_2D, bloomFboColor1, 0);
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1,
-	                       GL_TEXTURE_2D, bloomFboColor2, 0);
+	for (int i = 0; i < BLOOM_PASSES; i++)
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + i,
+		                       GL_TEXTURE_2D, bloomFboColor[i], 0);
 	if (glCheckFramebufferStatus(GL_FRAMEBUFFER)
 	    != GL_FRAMEBUFFER_COMPLETE) {
 		logCrit("Failed to initialize bloom framebuffer");
@@ -211,9 +210,9 @@ void initPostRenderer(void)
 
 void resizePostRender(int width, int height)
 {
-	bloomHeight = min(720, height);
+	bloomHeight = BLOOM_SIZE;
 	bloomWidth = (float)bloomHeight * ((float)width / (float)height);
-
+	
 	glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, renderFboColor);
 	glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, 4, GL_RGBA8,
 	                        width, height, GL_TRUE);
@@ -227,16 +226,13 @@ void resizePostRender(int width, int height)
 	glBindTexture(GL_TEXTURE_2D, resolveFboColor);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_BGRA,
 	             GL_UNSIGNED_BYTE, NULL);
-	glBindTexture(GL_TEXTURE_2D, 0);
 
-	glBindTexture(GL_TEXTURE_2D, bloomFboColor1);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16, bloomWidth, bloomHeight, 0,
-	             GL_BGRA, GL_UNSIGNED_BYTE, NULL);
-	glBindTexture(GL_TEXTURE_2D, 0);
-
-	glBindTexture(GL_TEXTURE_2D, bloomFboColor2);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16, bloomWidth, bloomHeight, 0,
-	             GL_BGRA, GL_UNSIGNED_BYTE, NULL);
+	for (int i = 0; i < BLOOM_PASSES; i++) {
+		glBindTexture(GL_TEXTURE_2D, bloomFboColor[i]);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, bloomWidth / (1 << i),
+		             bloomHeight / (1 << i), 0, GL_BGRA, GL_UNSIGNED_BYTE,
+		             NULL);
+	}
 	glBindTexture(GL_TEXTURE_2D, 0);
 
 	fboWidth = width;
@@ -245,10 +241,8 @@ void resizePostRender(int width, int height)
 
 void cleanupPostRenderer(void)
 {
-	glDeleteTextures(1, &bloomFboColor2);
-	bloomFboColor2 = 0;
-	glDeleteTextures(1, &bloomFboColor1);
-	bloomFboColor1 = 0;
+	glDeleteTextures(BLOOM_PASSES, bloomFboColor);
+	clearArray(bloomFboColor);
 	glDeleteFramebuffers(1, &bloomFbo);
 	bloomFbo = 0;
 	glDeleteTextures(1, &resolveFboColor);
@@ -337,17 +331,27 @@ void renderPostEnd(void)
 
 	// Draw blur
 	glUseProgram(blurProgram);
-	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, bloomFbo);
 
-	for (int i = 0; i < countof(blurKernel); i++) {
-		GLuint tx = (i % 2 == 0) ? (bloomFboColor1) : (bloomFboColor2);
-		GLenum buf = (i % 2 == 0) ? (GL_COLOR_ATTACHMENT1) : (GL_COLOR_ATTACHMENT0);
-		glDrawBuffer(buf);
+	for (int i = 1; i < BLOOM_PASSES; i++) {
+		glDrawBuffer(GL_COLOR_ATTACHMENT0 + i);
+		glViewport(0, 0, bloomWidth / (1 << i), bloomHeight / (1 << i));
 		glClear(GL_COLOR_BUFFER_BIT);
-		glBindTexture(GL_TEXTURE_2D, tx);
-		glUniform1i(stepAttr, blurKernel[i]);
+		glBindTexture(GL_TEXTURE_2D, bloomFboColor[i - 1]);
+		glUniform1f(stepAttr, 1.0f);
 		glDrawArrays(GL_TRIANGLES, 0, countof(vertexData) / 4);
 	}
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_ONE, GL_ONE);
+	for (int i = BLOOM_PASSES - 2; i >= 0; i--) {
+		glDrawBuffer(GL_COLOR_ATTACHMENT0 + i);
+		glViewport(0, 0, bloomWidth / (1 << i), bloomHeight / (1 << i));
+		glBindTexture(GL_TEXTURE_2D, bloomFboColor[i + 1]);
+		glUniform1f(stepAttr, 0.5f);
+		glDrawArrays(GL_TRIANGLES, 0, countof(vertexData) / 4);
+	}
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glDisable(GL_BLEND);
+	glDrawBuffer(GL_COLOR_ATTACHMENT0);
 
 	// Draw bloom
 	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
@@ -359,9 +363,7 @@ void renderPostEnd(void)
 	glBindTexture(GL_TEXTURE_2D, resolveFboColor);
 	glUniform1i(screenAttr, 0);
 	glActiveTexture(GL_TEXTURE1);
-	glBindTexture(GL_TEXTURE_2D,
-	              (countof(blurKernel) % 2 == 0) ? (bloomFboColor1)
-	                                             : (bloomFboColor2));
+	glBindTexture(GL_TEXTURE_2D, bloomFboColor[0]);
 	glUniform1i(bloomAttr, 1);
 	glUniform1f(bloomStrengthAttr, 0.4f);
 	glDrawArrays(GL_TRIANGLES, 0, countof(vertexData) / 4);
