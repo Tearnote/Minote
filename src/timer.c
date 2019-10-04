57e78f699f4416af98352f2a17cbd51e56c92a34
Hubert Maraszek <marach5@gmail.com>
2019-10-04 18:27:51 +0100

    Clean up timer.c

--- src/timer.c
+++ src/timer.c
@@ -2,17 +2,8 @@
 
 #include "timer.h"
 
-#ifdef WIN32
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-#include <winnt.h>
-#else // WIN32
-#include <pthread.h>
-#endif // WIN32
 #include "glad/glad.h"
 #include <GLFW/glfw3.h>
-
-#include "log.h"
 
 nsec getTime(void)
 {
