36230967f65d421b0e8d14e0d44b5519f8c14935
Tear♪ <marach5@gmail.com>
2020-09-16 17:01:22 +0100

    Improve msdf shader

--- src/glsl/msdf.frag.glsl
+++ src/glsl/msdf.frag.glsl
@@ -25,16 +25,17 @@ void main()
     vec3 s = texture(atlas, fTexCoords).rgb;
     float distance = (median(s.r, s.g, s.b) - 0.5) * pxRange;
     vec2 dxDist = vec2(dFdx(distance), dFdy(distance));
-    if (dxDist.x == 0.0 && dxDist.y == 0.0)
-        discard;
+    if (all(equal(vec2(0.0),dxDist)))
+        discard; // Avoid normalizing a zero vector
     vec2 gradDist = normalize(dxDist);
 
     vec2 uv = fTexCoords * textureSize(atlas, 0);
     vec2 Jdx = dFdx(uv);
     vec2 Jdy = dFdy(uv);
 
-    vec2 grad = vec2(gradDist.x * Jdx.x + gradDist.y*Jdy.x, gradDist.x * Jdx.y + gradDist.y*Jdy.y);
-    float afwidth = 0.7071*length(grad);
+    vec2 grad = vec2(gradDist.x * Jdx.x + gradDist.y * Jdy.x,
+                     gradDist.x * Jdx.y + gradDist.y * Jdy.y);
+    float afwidth = 0.7071 * length(grad);
     float coverage = 1.0 - smoothstep(afwidth, -afwidth, distance);
 
     outColor = vec4(fColor.rgb, smoothstep(0.0, fColor.a, coverage));
