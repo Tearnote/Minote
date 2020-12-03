eef5622972a0e588db8cbdf20b55518520c3343c
Tear♪ <marach5@gmail.com>
2020-12-03 02:12:24 +0000

    Start implementing ring_buffer

--- src/base/queue.hpp
+++ src/base/queue.hpp
@@ -4,52 +4,71 @@
 
 #pragma once
 
-#include "base/array.hpp"
-#include "base/util.hpp"
+#include <type_traits>
+#include <iterator>
+#include <limits>
 
 namespace minote {
 
-template<typename T, size_t N>
-struct queue {
+template<typename T, std::size_t Capacity>
+struct ring_buffer {
 
-	// Type of the queue element
-	using Element = T;
+	using value_type = T;
+	using reference = value_type&;
+	using const_reference = value_type const&;
+	struct iterator; // LegacyRandomAccessIterator
+	using const_iterator = iterator const;
+	using reverse_iterator = std::reverse_iterator<iterator>;
+	using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+	using difference_type = std::ptrdiff_t;
+	using size_type = std::size_t;
 
-	// Maximum number of elements in the queue plus one
-	static constexpr size_t Capacity = N;
+	constexpr ring_buffer(): offset{0}, length{0} {}
+	constexpr ring_buffer(ring_buffer const&) = default;
+	~ring_buffer() { clear(); };
 
-	// Ring buffer containing the elements
-	array<Element, Capacity> buffer = {};
+	constexpr auto size() const { return length; }
+	constexpr auto empty() const { return length == 0; }
+	constexpr auto capacity() const { return Capacity; }
+	constexpr auto max_size() const { return std::numeric_limits<size_type>::max(); }
 
-	// Index of the first empty slot to enqueue into
-	size_t head = 0;
+	auto at(size_type i) -> reference;
+	auto at(size_type i) const -> const_reference;
+	auto front() -> reference { return at(0); }
+	auto front() const -> const_reference { return at(0); }
+	auto back() -> reference { return at(length - 1); }
+	auto back() const -> const_reference { return at(length - 1); }
 
-	// Index of the next element to dequeue
-	size_t tail = 0;
+	auto begin() { return iterator{*this, 0}; }
+	auto begin() const { return const_iterator{*this, 0}; }
+	auto cbegin() const { return begin(); }
+	auto end() { return iterator{*this, size()}; }
+	auto end() const { return const_iterator{*this, size()}; }
+	auto cend() const { return end(); }
+	auto rbegin() { return reverse_iterator{end()}; }
+	auto rbegin() const { return const_reverse_iterator{end()}; }
+	auto crbegin() const { return rbegin(); }
+	auto rend() { return reverse_iterator{begin()}; }
+	auto rend() const { return const_reverse_iterator{begin()}; }
+	auto crend() const { return rend(); }
 
-	// Add an element to the back of the queue and return true. If there is
-	// no space, the element is not added and false is returned.
-	auto enqueue(Element const& e) -> bool;
+	void push_back(const_reference value);
+	void push_back(value_type&& value);
+	void push_front(const_reference value);
+	void push_front(value_type&& value);
+	void pop_front();
+	void pop_back();
+	void clear() { while (!empty()) pop_back(); }
 
-	// Remove and return an element from the front of the queue. If the queue is
-    // empty, nullptr is returned.
-	auto dequeue() -> Element*;
+	constexpr auto operator=(ring_buffer const&) -> ring_buffer& = default;
+	auto operator[](size_type const index) -> reference { return at(index); }
+	auto operator[](size_type const index) const -> const_reference { return at(index); }
 
-	// Return the element from the front of the queue without removing it.
-	// If the queue is empty, nullptr is returned.
-	auto peek() -> Element*;
-	auto peek() const -> Element const*;
+private:
 
-	// Return true if the queue is empty, false otherwise.
-	[[nodiscard]]
-	auto isEmpty() const -> bool;
-
-	// Return true if the queue is full, false otherwise.
-	[[nodiscard]]
-	auto isFull() const -> bool;
-
-	// Clear the queue, setting the number of elements to zero.
-	void clear();
+	typename std::aligned_storage<sizeof(value_type), alignof(value_type)> buffer[Capacity];
+	size_type offset;
+	size_type length;
 
 };
 
--- src/base/queue.tpp
+++ src/base/queue.tpp
@@ -1,63 +1,89 @@
 #pragma once
 
+#include <stdexcept>
+#include <utility>
+#include "base/util.hpp"
+#include "queue.hpp"
+
 namespace minote {
 
-template<typename T, size_t N>
-auto queue<T, N>::enqueue(Element const& e) -> bool
-{
-	if (isFull())
-		return false;
+template<typename T, std::size_t Capacity>
+struct ring_buffer<T, Capacity>::iterator {
 
-	buffer[head] = e;
-	head = (head + 1) % Capacity;
-	return true;
+	using parent_type = ring_buffer<T, Capacity>;
+
+	using iterator_category = std::random_access_iterator_tag;
+	using value_type = parent_type::value_type;
+	using difference_type = parent_type::difference_type;
+	using pointer = parent_type::value_type*;
+	using reference = parent_type::reference;
+
+};
+
+template<typename T, std::size_t Capacity>
+auto ring_buffer<T, Capacity>::at(size_type const i) -> reference {
+	ASSERT(i < length);
+	size_type const index = (offset + i) % capacity();
+	return std::launder(*reinterpret_cast<value_type*>(&buffer[index]));
 }
 
-template<typename T, size_t N>
-auto queue<T, N>::dequeue() -> Element*
-{
-	if (isEmpty())
-		return nullptr;
-
-	auto const prevTail = tail;
-	tail = (tail + 1) % Capacity;
-	return &buffer[prevTail];
+template<typename T, std::size_t Capacity>
+auto ring_buffer<T, Capacity>::at(size_type const i) const -> const_reference {
+	ASSERT(i < length);
+	size_type const index = (offset + i) % capacity();
+	return std::launder(*reinterpret_cast<value_type const*>(&buffer[index]));
 }
 
-template<typename T, size_t N>
-auto queue<T, N>::peek() -> Element*
-{
-	if (isEmpty())
-		return nullptr;
+template<typename T, size_t Capacity>
+void ring_buffer<T, Capacity>::push_back(const_reference value) {
+	if (length == capacity())
+		throw std::out_of_range{"ring_buffer is full"};
 
-	return &buffer[tail];
+	length += 1;
+	new(&back()) value_type{value};
 }
 
-template<typename T, size_t N>
-auto queue<T, N>::peek() const -> Element const*
-{
-	if (isEmpty())
-		return nullptr;
+template<typename T, size_t Capacity>
+void ring_buffer<T, Capacity>::push_back(value_type&& value) {
+	if (length == capacity())
+		throw std::out_of_range{"ring_buffer is full"};
 
-	return &buffer[tail];
+	length += 1;
+	new(&back()) value_type{std::move(value)};
 }
 
-template<typename T, size_t N>
-auto queue<T, N>::isEmpty() const -> bool
-{
-	return head == tail;
+template<typename T, std::size_t Capacity>
+void ring_buffer<T, Capacity>::push_front(const_reference value) {
+	if (length == capacity())
+		throw std::out_of_range{"ring_buffer is full"};
+
+	offset = offset? offset - 1 : capacity() - 1;
+	length += 1;
+	new(&front()) value_type{value};
 }
 
-template<typename T, size_t N>
-auto queue<T, N>::isFull() const -> bool
-{
-	return (head + 1) % Capacity == tail;
+template<typename T, std::size_t Capacity>
+void ring_buffer<T, Capacity>::push_front(value_type&& value) {
+	if (length == capacity())
+		throw std::out_of_range{"ring_buffer is full"};
+
+	offset = offset? offset - 1 : capacity() - 1;
+	length += 1;
+	new(&front()) value_type{std::move(value)};
 }
 
-template<typename T, size_t N>
-void queue<T, N>::clear()
-{
-	head = tail;
+template<typename T, size_t Capacity>
+void ring_buffer<T, Capacity>::pop_front() {
+	front().~value_type();
+	offset += 1;
+	offset %= capacity();
+	length -= 1;
+}
+
+template<typename T, std::size_t Capacity>
+void ring_buffer<T, Capacity>::pop_back() {
+	back().~value_type();
+	length -= 1;
 }
 
 }
