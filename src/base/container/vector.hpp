6b81a7833a7a9e5d841bdc9be59ee994ebe6aadd
Tear♪ <marach5@gmail.com>
2021-07-31 00:57:37 +0100

    Add a default ivector static length

--- src/base/container/vector.hpp
+++ src/base/container/vector.hpp
@@ -7,7 +7,7 @@
 namespace minote::base {
 
 // Inlined vector. Stored on stack initially, switches to heap above N elements.
-template<typename T, usize N, template<typename> typename Allocator = std::allocator>
+template<typename T, usize N = 256, template<typename> typename Allocator = std::allocator>
 using ivector = absl::InlinedVector<T, N, Allocator<T>>;
 
 // Static vector. Stored entirely on stack, throws if capacity is exceeded.
