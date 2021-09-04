fb3fc6f62860463aeacc0fc922e56d773fc41ce1
Hubert Maraszek <marach5@gmail.com>
2021-09-04 18:06:33 +0100

    Introduce Texture3D resource

---
+++ src/gfx/resources/texture3d.cpp
@@ -0,0 +1,45 @@
+#include "gfx/resources/texture3d.hpp"
+
+namespace minote::gfx {
+
+auto Texture3D::make(Pool& _pool, vuk::Name _name, uvec3 _size, vuk::Format _format,
+	vuk::ImageUsageFlags _usage) -> Texture3D {
+	
+	auto& texture = [&_pool, _name, _format, _size, _usage]() -> vuk::Texture& {
+		
+		if (_pool.contains(_name)) {
+			
+			return _pool.get<vuk::Texture>(_name);
+			
+		} else {
+			
+			return _pool.insert<vuk::Texture>(_name,
+				_pool.ptc().allocate_texture(vuk::ImageCreateInfo{
+					.format = _format,
+					.extent = {_size.x(), _size.y(), _size.z()},
+					.usage = _usage }));
+			
+		}
+		
+	}();
+	
+	return Texture3D{
+		.name = _name,
+		.handle = &texture };
+	
+}
+
+auto Texture3D::resource(vuk::Access _access) const -> vuk::Resource {
+	
+	return vuk::Resource(name, vuk::Resource::Type::eImage, _access);
+	
+}
+
+void Texture3D::attach(vuk::RenderGraph& _rg, vuk::Access _initial, vuk::Access _final,
+	vuk::Clear _clear) const {
+	
+	_rg.attach_image(name, vuk::ImageAttachment::from_texture(*handle, _clear), _initial, _final);
+	
+}
+
+}
