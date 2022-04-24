d5d406ecc72c6a088564638a0ad74c1799f426df
Tearnote <marach5@gmail.com>
2022-04-24 21:30:35 +0100

    Update fmt and Quill
    Fixes build error on gcc

--- src/base/error.hpp
+++ src/base/error.hpp
@@ -12,19 +12,19 @@ using std::runtime_error;
 using std::logic_error;
 
 template<typename Err, typename... Args>
-inline auto typed_error_fmt(std::string_view fmt, Args&&... args) -> Err {
+inline auto typed_error_fmt(fmt::format_string<Args...> fmt, Args&&... args) -> Err {
 	
 	return Err(format(fmt, std::forward<Args>(args)...));
 	
 }
 
 template<typename... Args>
-inline auto runtime_error_fmt(std::string_view fmt, Args&&... args) {
+inline auto runtime_error_fmt(fmt::format_string<Args...> fmt, Args&&... args) {
 	return typed_error_fmt<runtime_error>(fmt, std::forward<Args>(args)...);
 };
 
 template<typename... Args>
-inline auto logic_error_fmt(std::string_view fmt, Args&&... args) {
+inline auto logic_error_fmt(fmt::format_string<Args...> fmt, Args&&... args) {
 	return typed_error_fmt<logic_error>(fmt, std::forward<Args>(args)...);
 };
 
