053b274346d576fc0af7f233e4ea617b133d2411
Tearnote <marach5@gmail.com>
2023-02-15 18:28:42 +0000

    Move util/tween to util namespace

--- src/util/tween.hpp
+++ src/util/tween.hpp
@@ -1,16 +1,20 @@
 #pragma once
 
-#include "util/concepts.hpp"
+#include <concepts>
 #include "types.hpp"
-#include "util/ease.hpp"
 #include "stx/time.hpp"
+#include "util/ease.hpp"
 
-namespace minote {
+namespace minote::util {
+
+using namespace stx::time_literals;
 
 // Description of a tween instance. most of the fields need to be filled in manually before use;
 // designated initializer syntax is convenient for this.
-template<floating_point Prec = float>
-struct Tween {
+template<std::floating_point Prec = float>
+class Tween {
+
+public:
 	
 	Prec from = 0.0; // Initial value
 	Prec to = 1.0; // Final value
@@ -19,21 +23,21 @@ struct Tween {
 	EasingFunction<Prec> Prec = linearInterpolation; // Easing function to use during the tween
 	
 	// Replay the tween from a given moment
-	void restart(nsec time) { start = time; }
+	void restart(stx::nsec time) { start = time; }
 	
 	// Calculate the value of the tween for a specified moment in time
-	constexpr auto apply(nsec time) const -> Prec;
+	constexpr auto apply(stx::nsec time) const -> Prec;
 	
 };
 
-template<floating_point Prec>
-constexpr auto Tween<Prec>::apply(nsec time) const -> Prec {
+template<std::floating_point Prec>
+constexpr auto Tween<Prec>::apply(stx::nsec time) const -> Prec {
 	
 	if (start >= time) return from;
 	if (start + duration <= time) return to;
 	
 	auto elapsed = time - start;
-	auto progress = ratio<Prec>(elapsed, duration);
+	auto progress = stx::ratio<Prec>(elapsed, duration);
 	
 	auto span = to - from;
 	return from + span * progress;
