340764560d68fd4320b669a78d688cb5753f31f1
Tearnote <marach5@gmail.com>
2023-02-15 12:13:32 +0000

    Fix ease.hpp, put in util namespace

--- src/util/ease.hpp
+++ src/util/ease.hpp
@@ -1,16 +1,16 @@
 #pragma once
 
-#include "util/concepts.hpp"
+#include <concepts>
 #include "util/math.hpp"
 
-namespace minote {
+namespace minote::util {
 
 // Type matching all the below functions
-template<floating_point T>
+template<std::floating_point T>
 using EasingFunction = auto (*)(T) -> T;
 
 // Modeled after the line y = x
-template<floating_point T>
+template<std::floating_point T>
 constexpr auto linearInterpolation(T p) -> T {
 	
 	return p;
@@ -18,7 +18,7 @@ constexpr auto linearInterpolation(T p) -> T {
 }
 
 // Modeled after the parabola y = x^2
-template<floating_point T>
+template<std::floating_point T>
 constexpr auto quadraticEaseIn(T p) -> T {
 	
 	return p * p;
@@ -26,7 +26,7 @@ constexpr auto quadraticEaseIn(T p) -> T {
 }
 
 // Modeled after the parabola y = -x^2 + 2x
-template<floating_point T>
+template<std::floating_point T>
 constexpr auto quadraticEaseOut(T p) -> T {
 	
 	return -(p * (p - 2));
@@ -36,7 +36,7 @@ constexpr auto quadraticEaseOut(T p) -> T {
 // Modeled after the piecewise quadratic
 // y = (1/2)((2x)^2)             ; [0, 0.5)
 // y = -(1/2)((2x-1)*(2x-3) - 1) ; [0.5, 1]
-template<floating_point T>
+template<std::floating_point T>
 constexpr auto quadraticEaseInOut(T p) -> T {
 	
 	if (p < 0.5)
@@ -47,7 +47,7 @@ constexpr auto quadraticEaseInOut(T p) -> T {
 }
 
 // Modeled after the cubic y = x^3
-template<floating_point T>
+template<std::floating_point T>
 constexpr auto cubicEaseIn(T p) -> T {
 	
 	return p * p * p;
@@ -55,7 +55,7 @@ constexpr auto cubicEaseIn(T p) -> T {
 }
 
 // Modeled after the cubic y = (x - 1)^3 + 1
-template<floating_point T>
+template<std::floating_point T>
 constexpr auto cubicEaseOut(T p) -> T {
 	
 	auto f = p - 1;
@@ -66,7 +66,7 @@ constexpr auto cubicEaseOut(T p) -> T {
 // Modeled after the piecewise cubic
 // y = (1/2)((2x)^3)       ; [0, 0.5)
 // y = (1/2)((2x-2)^3 + 2) ; [0.5, 1]
-template<floating_point T>
+template<std::floating_point T>
 constexpr auto cubicEaseInOut(T p) -> T {
 	
 	if (p < 0.5) {
@@ -79,7 +79,7 @@ constexpr auto cubicEaseInOut(T p) -> T {
 }
 
 // Modeled after the quartic x^4
-template<floating_point T>
+template<std::floating_point T>
 constexpr auto quarticEaseIn(T p) -> T {
 	
 	return p * p * p * p;
@@ -87,7 +87,7 @@ constexpr auto quarticEaseIn(T p) -> T {
 }
 
 // Modeled after the quartic y = 1 - (x - 1)^4
-template<floating_point T>
+template<std::floating_point T>
 constexpr auto quarticEaseOut(T p) -> T {
 	
 	auto f = p - 1;
@@ -98,7 +98,7 @@ constexpr auto quarticEaseOut(T p) -> T {
 // Modeled after the piecewise quartic
 // y = (1/2)((2x)^4)        ; [0, 0.5)
 // y = -(1/2)((2x-2)^4 - 2) ; [0.5, 1]
-template<floating_point T>
+template<std::floating_point T>
 constexpr auto quarticEaseInOut(T p) -> T {
 	
 	if (p < 0.5) {
@@ -111,7 +111,7 @@ constexpr auto quarticEaseInOut(T p) -> T {
 }
 
 // Modeled after the quintic y = x^5
-template<floating_point T>
+template<std::floating_point T>
 constexpr auto quinticEaseIn(T p) -> T {
 	
 	return p * p * p * p * p;
@@ -119,7 +119,7 @@ constexpr auto quinticEaseIn(T p) -> T {
 }
 
 // Modeled after the quintic y = (x - 1)^5 + 1
-template<floating_point T>
+template<std::floating_point T>
 constexpr auto quinticEaseOut(T p) -> T {
 	
 	auto f = p - 1;
@@ -130,7 +130,7 @@ constexpr auto quinticEaseOut(T p) -> T {
 // Modeled after the piecewise quintic
 // y = (1/2)((2x)^5)       ; [0, 0.5)
 // y = (1/2)((2x-2)^5 + 2) ; [0.5, 1]
-template<floating_point T>
+template<std::floating_point T>
 constexpr auto quinticEaseInOut(T p) -> T {
 	
 	if (p < 0.5) {
@@ -143,7 +143,7 @@ constexpr auto quinticEaseInOut(T p) -> T {
 }
 
 // Modeled after quarter-cycle of sine wave
-template<floating_point T>
+template<std::floating_point T>
 constexpr auto sineEaseIn(T p) -> T {
 	
 	return sin((p - 1) * Pi_v<T> / 2) + 1;
@@ -151,7 +151,7 @@ constexpr auto sineEaseIn(T p) -> T {
 }
 
 // Modeled after quarter-cycle of sine wave (different phase)
-template<floating_point T>
+template<std::floating_point T>
 constexpr auto sineEaseOut(T p) -> T {
 	
 	return sin(p * Pi_v<T> / 2);
@@ -159,7 +159,7 @@ constexpr auto sineEaseOut(T p) -> T {
 }
 
 // Modeled after half sine wave
-template<floating_point T>
+template<std::floating_point T>
 constexpr auto sineEaseInOut(T p) -> T {
 	
 	return 0.5 * (1 - cos(p * Pi_v<T>));
@@ -167,7 +167,7 @@ constexpr auto sineEaseInOut(T p) -> T {
 }
 
 // Modeled after shifted quadrant IV of unit circle
-template<floating_point T>
+template<std::floating_point T>
 constexpr auto circularEaseIn(T p) -> T {
 	
 	return 1 - sqrt(1 - (p * p));
@@ -175,7 +175,7 @@ constexpr auto circularEaseIn(T p) -> T {
 }
 
 // Modeled after shifted quadrant II of unit circle
-template<floating_point T>
+template<std::floating_point T>
 constexpr auto circularEaseOut(T p) -> T {
 	
 	return sqrt((2 - p) * p);
@@ -185,7 +185,7 @@ constexpr auto circularEaseOut(T p) -> T {
 // Modeled after the piecewise circular function
 // y = (1/2)(1 - sqrt(1 - 4x^2))           ; [0, 0.5)
 // y = (1/2)(sqrt(-(2x - 3)*(2x - 1)) + 1) ; [0.5, 1]
-template<floating_point T>
+template<std::floating_point T>
 constexpr auto circularEaseInOut(T p) -> T {
 	
 	if (p < 0.5)
@@ -196,7 +196,7 @@ constexpr auto circularEaseInOut(T p) -> T {
 }
 
 // Modeled after the exponential function y = 2^(10(x - 1))
-template<floating_point T>
+template<std::floating_point T>
 constexpr auto exponentialEaseIn(T p) -> T {
 	
 	return (p == 0.0)? p : pow(2, 10 * (p - 1));
@@ -204,7 +204,7 @@ constexpr auto exponentialEaseIn(T p) -> T {
 }
 
 // Modeled after the exponential function y = -2^(-10x) + 1
-template<floating_point T>
+template<std::floating_point T>
 constexpr auto exponentialEaseOut(T p) -> T {
 	
 	return (p == 1.0)? p : 1 - pow(2, -10 * p);
@@ -214,7 +214,7 @@ constexpr auto exponentialEaseOut(T p) -> T {
 // Modeled after the piecewise exponential
 // y = (1/2)2^(10(2x - 1))         ; [0,0.5)
 // y = -(1/2)*2^(-10(2x - 1))) + 1 ; [0.5,1]
-template<floating_point T>
+template<std::floating_point T>
 constexpr auto exponentialEaseInOut(T p) -> T {
 	
 	if (p == 0.0 || p == 1.0)
@@ -228,7 +228,7 @@ constexpr auto exponentialEaseInOut(T p) -> T {
 }
 
 // Modeled after the damped sine wave y = sin(13pi/2*x)*pow(2, 10 * (x - 1))
-template<floating_point T>
+template<std::floating_point T>
 constexpr auto elasticEaseIn(T p) -> T {
 	
 	return sin(13 * (Pi_v<T> / 2) * p) * pow(2, 10 * (p - 1));
@@ -236,7 +236,7 @@ constexpr auto elasticEaseIn(T p) -> T {
 }
 
 // Modeled after the damped sine wave y = sin(-13pi/2*(x + 1))*pow(2, -10x) + 1
-template<floating_point T>
+template<std::floating_point T>
 constexpr auto elasticEaseOut(T p) -> T {
 	
 	return sin(-13 * (Pi_v<T> / 2) * (p + 1)) * pow(2, -10 * p) + 1;
@@ -246,7 +246,7 @@ constexpr auto elasticEaseOut(T p) -> T {
 // Modeled after the piecewise exponentially-damped sine wave:
 // y = (1/2)*sin(13pi/2*(2*x))*pow(2, 10 * ((2*x) - 1))      ; [0,0.5)
 // y = (1/2)*(sin(-13pi/2*((2x-1)+1))*pow(2,-10(2*x-1)) + 2) ; [0.5, 1]
-template<floating_point T>
+template<std::floating_point T>
 constexpr auto elasticEaseInOut(T p) -> T {
 	
 	if (p < 0.5) {
@@ -260,7 +260,7 @@ constexpr auto elasticEaseInOut(T p) -> T {
 }
 
 // Modeled after the overshooting cubic y = x^3-x*sin(x*pi)
-template<floating_point T>
+template<std::floating_point T>
 constexpr auto backEaseIn(T p) -> T {
 	
 	return p * p * p - p * sin(p * Pi_v<T>);
@@ -268,7 +268,7 @@ constexpr auto backEaseIn(T p) -> T {
 }
 
 // Modeled after overshooting cubic y = 1-((1-x)^3-(1-x)*sin((1-x)*pi))
-template<floating_point T>
+template<std::floating_point T>
 constexpr auto backEaseOut(T p) -> T {
 	
 	auto f = 1 - p;
@@ -279,7 +279,7 @@ constexpr auto backEaseOut(T p) -> T {
 // Modeled after the piecewise overshooting cubic function:
 // y = (1/2)*((2x)^3-(2x)*sin(2*x*pi))           ; [0, 0.5)
 // y = (1/2)*(1-((1-x)^3-(1-x)*sin((1-x)*pi))+1) ; [0.5, 1]
-template<floating_point T>
+template<std::floating_point T>
 constexpr auto backEaseInOut(T p) -> T {
 	
 	if (p < 0.5) {
@@ -292,14 +292,14 @@ constexpr auto backEaseInOut(T p) -> T {
 	
 }
 
-template<floating_point T>
+template<std::floating_point T>
 constexpr auto bounceEaseIn(T p) -> T {
 	
 	return 1 - bounceEaseOut(1 - p);
 	
 }
 
-template<floating_point T>
+template<std::floating_point T>
 constexpr auto bounceEaseOut(T p) -> T {
 	
 	if (p < 4 / 11.0)
@@ -313,7 +313,7 @@ constexpr auto bounceEaseOut(T p) -> T {
 	
 }
 
-template<floating_point T>
+template<std::floating_point T>
 constexpr auto bounceEaseInOut(T p) -> T {
 	
 	if (p < 0.5)
